1fb6d0a0e57324f4257ecc4bbe9be9ab85192f29
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2016-11-06 11:24:04 +0100

    Upgraded data to 1.1.16

--- data/get_data.py
+++ data/get_data.py
@@ -11,7 +11,7 @@ server.retrieve({
     'step'      : "0",
     'grid'      : "0.75/0.75",
     'time'      : "00",
-    'date'      : "2015-10-01/to/2015-10-01",
+    'date'      : "2016-01-01/to/2016-01-01",
     'type'      : "an",
     'class'     : "ei",
     'format'    : "netcdf",
